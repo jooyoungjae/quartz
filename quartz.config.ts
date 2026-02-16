2f3ff3ade7210bb636690a455847fa31a085fba9
jooyoung <plasd999@gmail.com>
2026-02-16 18:40:56 +0900

    Quartz sync: Feb 16, 2026, 6:40 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "plasd999.github.io/quartz",
+    baseUrl: "jooyoungjae.github.io/quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
