610492ffcc93a6403194bd6c1655fd8bf0b455ff
jooyoung <plasd999@gmail.com>
2026-02-17 11:35:23 +0900

    Quartz sync: Feb 17, 2026, 11:35 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Noto Serif Korean",
+        body: "Noto Serif Korean",
         code: "IBM Plex Mono",
       },
       colors: {
